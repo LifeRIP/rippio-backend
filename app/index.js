ad6ef485de7e6c86931c553bb9a77bffb58f6189
Zers04 <juandacata04@gmail.com>
2024-04-28 13:46:22 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -8,4 +8,4 @@ app.listen(app.get("port"));
 console.log("servidor corriendo en el puerto", app.get("port"));
 
 //Rutas
-app.get("/",(req,res)=> res.sendFile(__dirname + "/pages/login.html"))
+app.get("/login",(req,res)=> res.sendFile(__dirname + "/pages/login.html"))
