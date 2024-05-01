0eba6a7ba6837746f27709177e92a9ca8e1c8cbf
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-05-01 16:50:27 -0500

    Fixes auth controller

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -94,33 +94,52 @@ async function register(req, res) {
 }
 
 async function login(req, res) {
-  const email = req.body.email;
-  const password = req.body.password;
-  if (!email || !password) {
-    return res
-      .status(400)
-      .send({ status: "Error", message: "Los campos estan incompletos" });
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    if (!email || !password) {
+      return res.status(400).send({
+        status: "Error",
+        message: "Los campos estan incompletos",
+      });
+    }
+
+    // Validar que el usuario exista
+    const user = await pool.query(
+      "SELECT * FROM datos_usuarios where email = $1",
+      [email]
+    );
+
+    if (user.rows.length === 0) {
+      return res.status(401).send({
+        status: "Error",
+        message: "Error de inicio de sesion",
+      });
+    }
+
+    // Validar la contraseña
+    const PasswordValid = await bcrypt.compare(
+      password,
+      user.rows[0].contraseña
+    );
+
+    if (!PasswordValid) {
+      return res.status(401).send({
+        status: "Error",
+        message: "Contraseña Incorrecta",
+      });
+    }
+
+    //TODO: Crear token de autenticación
+    res.status(200);
+
+    console.log("Inicio de Sesion exitoso");
+  } catch (error) {
+    res.status(500).json({
+      status: "Error",
+      message: "Ha ocurrido un error al iniciar sesion",
+    });
   }
-  const user = await pool.query(
-    "SELECT * FROM datos_usuarios where email = $1",
-    [email]
-  );
-
-  if (user.rows.length === 0) {
-    return res
-      .status(401)
-      .send({ status: "Error", message: "Error de inicio de sesion" });
-  }
-
-  const PasswordValid = await bcrypt.compare(password, user.rows[0].contraseña);
-
-  if (!PasswordValid) {
-    return res
-      .status(401)
-      .send({ status: "Error", message: "Contraseña Incorrecta" });
-  }
-
-  console.log("Inicio de Sesion exitoso");
 }
 
 module.exports = { register, login };
