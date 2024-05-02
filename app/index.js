5a59a804c162933b0fdabf30b08c148d1208a89d
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-05-01 20:45:21 -0500

    New structure for routes

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { register, login } = require("./controllers/auth.controller");
+//const { register, login } = require("./controllers/auth.controller");
+const { router } = require("./routes/routes.js");
 
 // Server
-
 const app = express();
 app.set("port", 4000);
 app.listen(app.get("port"));
@@ -15,5 +15,8 @@ app.use(express.json()); // Para que express pueda entender los datos que vienen
 // Rutas
 app.get("/", (req, res) => res.sendFile(__dirname + "/pages/login.html"));
 app.get("/login", (req, res) => res.sendFile(__dirname + "/pages/login.html"));
-app.post("/api/login", login);
-app.post("/api/register", register);
+
+// API
+app.use("/api", router);
+/* app.post("/api/login", login);
+app.post("/api/register", register); */
