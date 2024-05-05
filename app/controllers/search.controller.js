46e553b5109a200de68705f5ef3ea72551b0c3f7
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-05-05 00:27:12 -0500

    feat: Add search functionality to retrieve restaurants and their products
    The code changes in the `search.controller.js` file add a new function `search` that allows users to search for restaurants and their products based on a given request. The function executes a SQL query to retrieve the relevant data and returns it as a JSON response. Additionally, the `search.js` file sets up the corresponding route for the search API endpoint.
    
    These changes allow users to search for restaurants by name or description, and retrieve the results as JSON.

---
+++ app/controllers/search.controller.js
@@ -0,0 +1,35 @@
+const { pool } = require("../database/dbConfig");
+
+async function search(req, res) {
+  try {
+    const request = req.body.request;
+    const response = await pool.query(
+      //TODO: para buscar solo los productos activos agregar en el WHERE -> AND p.estado = true
+      `SELECT
+      r.id,
+      du.nombre,
+      r.calificacion,
+      du.img_icon,
+      json_agg(
+          json_build_object(
+              'id', p.id,
+              'estado', p.estado,
+              'nombre', p.nombre,
+              'descripcion', p.descripcion,
+              'img_product', p.img_product,
+              'costo_unit', p.cost_unit
+          )
+      ) AS productos
+      FROM producto p
+      JOIN datos_usuarios du ON du.id = p.id_restaurante
+      JOIN restaurante r ON r.id = du.id
+      WHERE p.nombre ILIKE '%' || '${request}' || '%' OR p.descripcion ILIKE '%' || '${request}' || '%'
+      GROUP BY r.id, du.nombre, r.calificacion, du.img_icon;`
+    );
+    res.json(response.rows);
+  } catch (error) {
+    res.status(500).json({ error: "Ha ocurrido un error al obtener los restaurantes" });
+  }
+}
+
+module.exports = { search };
