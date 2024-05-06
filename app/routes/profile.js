d52c7a9d8699776b956c06ffb65e5bfc6714d4b4
Zers04 <juandacata04@gmail.com>
2024-05-05 20:18:17 -0500

    Implement functions edit data user
    The modification of the name, last name, phone number, adding address, and changing password was added.

---
+++ app/routes/profile.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const auth_user = require("../middleware/auth.user.middleware")
+const {change_data,change_password,add_address} = require("../controllers/edit.profile.controller");
+
+router.post("/edit",auth_user,change_data); // /api/profile/edit
+router.post("/change_password",auth_user,change_password); // /api/profile/change_password
+router.post("/add_address",auth_user,add_address); // /api/profile/add_address
+
+
+
+module.exports = router;
