ae0b848a0f82b0f7c8b77e44d81f67fdfbd31791
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-05-08 13:23:38 -0500

    refactor: Update CORS configuration to allow specific origins
    The code changes in this commit update the CORS configuration in the `index.js` file to allow specific origins for cross-origin resource sharing. The allowed origins include `http://localhost:4000`, `http://localhost:5173`, and `https://rippio.netlify.app`. This ensures that requests from these origins are allowed by the server, while requests from other origins are rejected. This improves security and restricts access to the server resources.

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
 const express = require('express');
 const { router } = require('./app/routes/index');
 const path = require('path');
+const cors = require('cors');
 
 // Server
 const app = express();
-app.set('port', 4000);
-app.listen(app.get('port'));
-console.log(`Servidor corriendo en http://localhost:${app.get('port')}/`);
+const port = process.env.PORT || 4000;
+app.listen(port);
+console.log(`Servidor corriendo en http://localhost:${port}/`);
 
 // Configuracion
-app.use(express.static(path.join(__dirname, 'public'))); // Rutas de prueba
+app.use(express.static(path.join(__dirname, '/app/public'))); // Rutas de prueba
 app.use(express.json()); // Para que express pueda entender los datos que vienen del cliente
+
+// Cors
+const corsOptions = {
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      'http://localhost:4000',
+      'http://localhost:5173',
+      'https://rippio.netlify.app',
+    ];
+
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed'), false);
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 // Rutas API
 app.use('/api', router);
 
 // Rutas de pruebas para el login
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/pages/login.html'))
+  res.sendFile(path.join(__dirname, '/app/pages/login.html'))
 );
 app.get('/forgot-password', (req, res) =>
-  res.sendFile(path.join(__dirname, '/pages/forgot-password.html'))
+  res.sendFile(path.join(__dirname, '/app/pages/forgot-password.html'))
 );
 app.get('/reset-password', (req, res) =>
-  res.sendFile(path.join(__dirname, '/pages/reset-password.html'))
+  res.sendFile(path.join(__dirname, '/app/pages/reset-password.html'))
 );
 app.get('/login', (req, res) =>
-  res.sendFile(path.join(__dirname, '/pages/login.html'))
+  res.sendFile(path.join(__dirname, '/app/pages/login.html'))
 );
