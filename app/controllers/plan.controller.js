e710026ee4913d526052fd3d8f49f9316c3488ae
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-05-08 13:35:07 -0500

    feat: Create plan API endpoint `api/plan/getAll` to retrieve all plans

---
+++ app/controllers/plan.controller.js
@@ -0,0 +1,17 @@
+const { pool } = require('../database/dbConfig');
+
+async function getAll(req, res) {
+  try {
+    const response = await pool.query(
+      `SELECT * 
+      FROM plan`
+    );
+    res.json(response.rows);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: 'Ha ocurrido un error al obtener los planes' });
+  }
+}
+
+module.exports = { getAll };
