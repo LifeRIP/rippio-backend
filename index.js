3dc343ad2298cebbde3fd539940012961f50924d
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-05-09 16:25:27 -0500

    feat: Add new allowed origin for CORS

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const corsOptions = {
   origin: function (origin, callback) {
     const allowedOrigins = [
       'http://localhost:4000',
+      'https://rippio-api.vercel.app',
       'http://localhost:5173',
       'https://rippio.netlify.app',
     ];
