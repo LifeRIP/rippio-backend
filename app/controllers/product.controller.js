b992b06b130e4f1663051b7a808ac692987be642
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-05-10 14:26:25 -0500

    feat: Add API endpoint `/api/product/getByResProd/:id` to retrieve products by restaurant id in the route, and product name in the body

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -24,4 +24,33 @@ async function getByResID(req, res) {
   }
 }
 
-module.exports = { getByResID };
+async function getByResProd(req, res) {
+  try {
+    const id = req.params.id;
+    const nombre = req.body.nombre;
+
+    // REPLACE junto con los espacios entre comillas simples quita los espacio de la búsqueda
+    // LOWER convierte en minusculas la busqueda y la info en la base de datos para que coincidan
+    const response = await pool.query(
+      `SELECT * 
+      FROM producto 
+      WHERE id_restaurante = $1 AND REPLACE(UNACCENT(LOWER(nombre)), ' ', '') 
+      LIKE REPLACE(UNACCENT(LOWER($2)), ' ', '')`,
+      [id, `%${nombre}%`]
+    );
+
+    if (response.rows.length === 0) {
+      return res
+        .status(404)
+        .json({ error: 'No se encontraron productos para el restaurante' });
+    }
+
+    res.json(response.rows);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: 'Ha ocurrido un error al obtener los productos' });
+  }
+}
+
+module.exports = { getByResID, getByResProd };
