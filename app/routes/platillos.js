f5e9fa3dd1245f8b706418f390960ce463d60588
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-05-10 14:49:10 -0500

    refactor: Deprecated API endpoints for retrieving products by restaurant name

--- app/routes/platillos.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getByResName } = require('../controllers/platillos.controller');
-
-router.get('/getByResName/:id', getByResName); // /api/platillos/getByResName/:id
-
-module.exports = router;
