14e4e30b7c42ac01695bb2666842ae0fda12e2df
Zers04 <juandacata04@gmail.com>
2024-05-20 08:14:07 -0500

    fix platillos

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -28,15 +28,18 @@ async function getByResProd(req, res) {
   try {
     const id = req.params.id;
     const nombre = req.body.nombre;
+    // Seleccionar todos los productos de un restaurante por nombre
 
-    // REPLACE junto con los espacios entre comillas simples quita los espacio de la búsqueda
-    // LOWER convierte en minusculas la busqueda y la info en la base de datos para que coincidan
     const response = await pool.query(
-      `SELECT * 
-      FROM producto 
-      WHERE id_restaurante = $1 AND REPLACE(UNACCENT(LOWER(nombre)), ' ', '') 
-      LIKE REPLACE(UNACCENT(LOWER($2)), ' ', '')`,
-      [id, `%${nombre}%`]
+      `SELECT p.id, p.nombre, p.descripcion, p.cost_unit, p.img_product, p.estado
+      FROM producto p  JOIN restaurante r ON r.id = p.id_restaurante
+      WHERE LOWER(REPLACE(p.nombre, ' ', '')) ILIKE LOWER(REPLACE($2, ' ', ''))
+      AND r.id = $1`,
+
+      // LOWER convierte en minusculas la busqueda y la info en la base de datos para que coincidan   
+      //REPLACE juntio con los espacios entre comillas simple quita los espacio de la búsqueda  
+      
+      [id, `%${nombre}%`] 
     );
 
     if (response.rows.length === 0) {
@@ -44,7 +47,7 @@ async function getByResProd(req, res) {
         .status(404)
         .json({ error: 'No se encontraron productos para el restaurante' });
     }
-
+    
     res.json(response.rows);
   } catch (error) {
     res
