68bd8b42c7374959c47f206077185d28aa759860
Zers04 <juandacata04@gmail.com>
2024-05-19 21:13:32 -0500

    fix platillos.js

--- app/controllers/platillos.controller.js
+++ app/controllers/platillos.controller.js
@@ -4,12 +4,16 @@ async function getByResName(req, res) {
   try {
     const id = req.params.id;
     const nombre = req.body.nombre;
-    console.log(id, nombre);
+    // Seleccionar todos los productos de un restaurante por nombre
+
     const response = await pool.query(
-      `SELECT * 
-      FROM producto 
-      WHERE id_restaurante = $1 AND REPLACE(UNACCENT(LOWER(nombre)), ' ', '') LIKE REPLACE(UNACCENT(LOWER($2)), ' ', '')`, //REPLACE juntio con los espacios entre comillas simple quita los espacio de la búsqueda
-                                                                                                                        // LOWER convierte en minusculas la busqueda y la info en la base de datos para que coincidan
+      `SELECT p.id, p.nombre, p.descripcion, p.cost_unit, p.img_product, p.estado
+      FROM producto p  JOIN restaurante r ON r.id = p.id_restaurante
+      WHERE LOWER(REPLACE(p.nombre, ' ', '')) ILIKE LOWER(REPLACE($2, ' ', ''))
+      AND r.id = $1`,
+
+      // LOWER convierte en minusculas la busqueda y la info en la base de datos para que coincidan   
+      //REPLACE juntio con los espacios entre comillas simple quita los espacio de la búsqueda                                                                                                         
       [id, `%${nombre}%`] 
     );
 
