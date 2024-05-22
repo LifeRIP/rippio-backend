9514bee0c9fb496a84e82a4065bccf8a2d4b4aaa
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-05-22 16:42:17 -0500

    feat: Add user_data controller and routes
    The code changes add a new controller file `user_data.controller.js` and corresponding routes in `user_data.js`. The `user_data.controller.js` file contains two functions: `getAll` and `getById`, which handle retrieving user data from the database. The `user_data.js` file sets up the routes for accessing these functions. This change adds functionality for retrieving user data, improving the overall functionality of the application.

---
+++ app/controllers/user_data.controller.js
@@ -0,0 +1,39 @@
+const { pool } = require('../database/dbConfig');
+
+async function getAll(req, res) {
+  try {
+    const response = await pool.query(
+      `SELECT * 
+      FROM datos_usuarios`
+    );
+    res.json(response.rows);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: 'Ha ocurrido un error al obtener los planes' });
+  }
+}
+
+async function getById(req, res) {
+  try {
+    const id = req.params.id;
+    const response = await pool.query(
+      `SELECT * 
+      FROM datos_usuarios
+      WHERE id = $1`,
+      [id]
+    );
+
+    if (response.rows.length === 0) {
+      return res.status(404).json({ error: 'No se encontraron usuarios' });
+    }
+
+    res.json(response.rows);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: 'Ha ocurrido un error al obtener los planes' });
+  }
+}
+
+module.exports = { getAll, getById };
