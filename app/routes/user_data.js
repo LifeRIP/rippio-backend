dbbfbcf71ba3e09af49b8f9a93a3abbabe47c6a0
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-05-22 16:47:36 -0500

    refactor: Update user_data route to include ID parameter in getById endpoint

--- app/routes/user_data.js
+++ app/routes/user_data.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { getAll, getById } = require('../controllers/user_data.controller');
 
 router.get('/getAll', getAll); // /api/user_data/getAll
-router.get('/getById', getById); // /api/user_data/getById
+router.get('/getById/:id', getById); // /api/user_data/getById
 
 module.exports = router;
