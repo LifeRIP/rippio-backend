d449ff7f4b1ff06ae2b7ed04cd52729f00dd52dd
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-05-23 23:26:10 -0500

    chore: Remove unused menu routes

--- app/routes/product.js
+++ app/routes/product.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth.user.middleware');
 const {
   getByResID,
   getByResProd,
   AggProd,
-  InhabProd
+  InhabProd,
 } = require('../controllers/product.controller');
 
 router.get('/getByResID/:id', getByResID); // /api/product/getByResID/:id
 router.get('/getByResProd/:id', getByResProd); // /api/product/getByResProd/:id
 router.post('/AggMenu', auth, AggProd); // /api/product/AggProd
-router.post('/InhabMenu', auth, InhabProd);// /api/product/InhabProd
+router.post('/InhabMenu', auth, InhabProd); // /api/product/InhabProd
 
 module.exports = router;
