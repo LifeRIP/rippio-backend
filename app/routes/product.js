f08720b828f6667ef75f491631159ed5ef7c13d7
Zers04 <juandacata04@gmail.com>
2024-05-23 21:43:30 -0500

    Fix routes

--- app/routes/product.js
+++ app/routes/product.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const {
   getByResID,
   getByResProd,
+  AggMenu,
+  InhabMenu
 } = require('../controllers/product.controller');
 
 router.get('/getByResID/:id', getByResID); // /api/product/getByResID/:id
 router.get('/getByResProd/:id', getByResProd); // /api/product/getByResProd/:id
+router.post('/AggMenu', auth, AggMenu); // /api/product/AggMenu
+router.post('/InhabMenu', auth, InhabMenu);// /api/product/InhabMenu
 
 module.exports = router;
