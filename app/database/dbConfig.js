52480ac8ac5ace198113b6fe5c3c2e7238134fea
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-05-24 21:57:03 -0500

    feat: Improve database connection reliability
    Add automatic reconnection functionality to the database configuration in `dbConfig.js`. This ensures that the application can recover from temporary database connection issues, improving its reliability.

--- app/database/dbConfig.js
+++ app/database/dbConfig.js
@@ -2,16 +2,27 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
-  // Intenta reconectar en caso de pérdida de conexión
-  reconnect: true,
 });
 
-pool.connect((err) => {
-  if (err) {
-    console.error('Error al conectar con la base de datos', err.stack);
-  } else {
-    console.error('Conexión a la base de datos exitosa');
-  }
+pool.on('error', (err, client) => {
+  console.error('Error inesperado en cliente inactivo', err);
+  process.exit(-1);
 });
+
+let firstConnection = true;
+
+function connectToDb() {
+  pool.connect((err, client, done) => {
+    if (err) throw err;
+    if (firstConnection) {
+      console.log('Conexión exitosa a la base de datos');
+      firstConnection = false;
+    }
+    client.on('end', connectToDb); // Reintentar la conexión si se pierde
+    client.on('error', connectToDb); // Reintentar la conexión si hay un error
+  });
+}
+
+connectToDb();
 
 module.exports = { pool };
