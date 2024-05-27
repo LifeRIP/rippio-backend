8736889661d0c2f2d2bdda5715600958b023b278
DanilsGit <danielestebanmarquez12@gmail.com>
2024-05-27 00:22:47 -0500

    fix

--- app/routes/restaurant.js
+++ app/routes/restaurant.js
@@ -4,12 +4,11 @@ const {
   getTopByCity,
   getById,
   getCatAndProdByResId,
-  getProfileById,
 } = require('../controllers/restaurant.controller');
 
 router.get('/getTopByCity', getTopByCity); // /api/restaurant/getTopByCity
 router.get('/getById/:id', getById); // /api/restaurant/getById
-router.get('/getProfileById/:id', getProfileById); // /api/restaurant/getProfileById
+router.get('/getProfileById/:id'); // /api/restaurant/getProfileById
 router.get('/getCatAndProdByResId/:id', getCatAndProdByResId); // /api/restaurant/getCatAndProdByResId
 
 module.exports = router;
