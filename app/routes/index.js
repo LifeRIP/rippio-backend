e7ac7d7b5d6dc114542822d020760a9b5c2a26e6
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-05-27 21:38:48 -0500

    chore: Update routes to use forEach instead of filter
    This commit updates the code in the `index.js` file to use the `forEach` method instead of the `filter` method when iterating over the `files` array. This change ensures that all files in the directory are processed and routes are created for each file, except for the `index.js` file itself.
    
    This commit message follows the established convention of using a verb in the imperative form to describe the purpose of the code changes.

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,7 +6,7 @@ const path = require('node:path');
 const files = fs.readdirSync(__dirname); // Lee los archivos de la carpeta actual
 
 // Filtra los archivos que no sean el actual y crea las rutas dinamicas
-files.filter((file) => {
+files.forEach((file) => {
   const name = path.basename(file, '.js');
   if (name === 'index') return;
   router.use(`/${name}`, require(`./${file}`)); // Importa las rutas de los archivos
