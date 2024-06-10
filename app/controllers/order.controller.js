9d5ab0f9938f99a4d2989f9a2bfc5eea8a89ec30
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-06-10 13:09:17 -0500

    fix: Order credits

--- app/controllers/order.controller.js
+++ app/controllers/order.controller.js
@@ -3,45 +3,42 @@ const { pool } = require('../database/dbConfig');
 
 async function add_order(req, res) {
   try {
-
     const { id } = req.user;
 
-    const {  
-      id_payment_method,
-      id_address,
-      use_credits,
-      shipping_cost,
-      date
-    } = req.body;
+    const { id_payment_method, id_address, use_credits, shipping_cost, date } =
+      req.body;
 
-    let total_cost ;
-    let credits_used ;
-    let credits_result ;
+    let total_cost;
+    let credits_used;
+    let credits_result;
 
     //Validar que los campos no estén vacíos
-    if (
-      !id_payment_method ||
-      !id_address ||
-      !shipping_cost ||
-      !date
-    ) {
-      return res.status(400).json({ error: 'Por favor complete todos los campos' });
+    if (!id_payment_method || !id_address || !shipping_cost || !date) {
+      return res
+        .status(400)
+        .json({ error: 'Por favor complete todos los campos' });
     }
 
     // Validar que use_credits no sea undefined
 
     if (use_credits === undefined) {
-      return res.status(400).json({ error: 'El campo use_credits es requerido' });
+      return res
+        .status(400)
+        .json({ error: 'El campo use_credits es requerido' });
     }
 
     // Validar que el campo use_credits sea booleano
     if (typeof use_credits !== 'boolean') {
-      return res.status(400).json({ error: 'El campo use_credits debe ser booleano' });
+      return res
+        .status(400)
+        .json({ error: 'El campo use_credits debe ser booleano' });
     }
 
     // Validar que el campo shipping_cost sea un número positivo
     if (typeof shipping_cost !== 'number' || shipping_cost <= 0) {
-      return res.status(400).json({ error: 'El campo shipping_cost debe ser un número positivo' });
+      return res
+        .status(400)
+        .json({ error: 'El campo shipping_cost debe ser un número positivo' });
     }
 
     // Verificar si el carrito del usuario no está vacío
@@ -52,9 +49,7 @@ async function add_order(req, res) {
     );
 
     if (cart.rows.length === 0) {
-      return res
-        .status(400)
-        .json({ error: 'No hay productos en el carrito' });
+      return res.status(400).json({ error: 'No hay productos en el carrito' });
     }
 
     // Verificar si la dirección del usuario existe
@@ -65,9 +60,7 @@ async function add_order(req, res) {
     );
 
     if (address.rows.length === 0) {
-      return res
-        .status(400)
-        .json({ error: 'La dirección no existe' });
+      return res.status(400).json({ error: 'La dirección no existe' });
     }
 
     // Verificar si el método de pago del usuario existe
@@ -78,56 +71,51 @@ async function add_order(req, res) {
     );
 
     if (payment_method.rows.length === 0) {
-      return res
-        .status(400)
-        .json({ error: 'El método de pago no existe' });
+      return res.status(400).json({ error: 'El método de pago no existe' });
     }
 
     // Calcular costo total del pedido
-    
+
     const total = await pool.query(
       `SELECT sum(cost_unit * cantidad_prod) as Costo_Total
       FROM carrito C join producto P on C.id_producto = P.id
       Where C.id_usuario = $1`,
       [id]
     );
-    
+
     //Obtener cantidad de creditos del usuario
-    
+
     const credits = await pool.query(
       `SELECT creditos FROM datos_usuarios WHERE id = $1`,
       [id]
     );
 
     if (use_credits) {
-      
       if (credits.rows[0].creditos <= 0) {
         return res.status(400).json({ error: 'No puedes usar créditos' });
       }
 
-      total_cost = Number(total.rows[0].costo_total) - Number(credits.rows[0].creditos) + Number(shipping_cost);
+      total_cost =
+        Number(total.rows[0].costo_total) -
+        Number(credits.rows[0].creditos) +
+        Number(shipping_cost);
 
       if (total_cost < 0) {
-
         credits_used = Number(credits.rows[0].creditos) + total_cost;
         credits_result = Math.floor(total_cost * -1);
         total_cost = 0;
-        	
-
-      }else {
+      } else {
         credits_used = Number(credits.rows[0].creditos);
 
-        credits_result = total_cost*0.1;
+        credits_result = Math.floor(total_cost * 0.1);
       }
-
     } else {
-
       total_cost = Number(total.rows[0].costo_total) + Number(shipping_cost);
 
       credits_used = 0;
 
-      credits_result = Math.floor(total_cost * 0.1) + Number(credits.rows[0].creditos);
-
+      credits_result =
+        Math.floor(total_cost * 0.1) + Number(credits.rows[0].creditos);
     }
 
     //Obtener el id del restaurante
@@ -139,47 +127,59 @@ async function add_order(req, res) {
       [id]
     );
 
-    const id_restaurant = restaurant.rows[0].id_restaurante
+    const id_restaurant = restaurant.rows[0].id_restaurante;
 
-  
     // Crear el pedido
     const newOrder = await pool.query(
       `INSERT INTO pedido (id_usuario, id_restaurante,  id_direccion, id_detalles_metodo_pago, estado, fecha, costo_total, costo_envio, creditos_usados) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
-      [id, id_restaurant, id_address, id_payment_method, 'Pendiente',  date, total_cost, shipping_cost, credits_used]
-    );  
-    
+      [
+        id,
+        id_restaurant,
+        id_address,
+        id_payment_method,
+        'Pendiente',
+        date,
+        total_cost,
+        shipping_cost,
+        credits_used,
+      ]
+    );
+
     // Crear el detalle del pedido
     for (let i = 0; i < cart.rows.length; i++) {
       const Product_Value = await pool.query(
         `SELECT cost_unit FROM producto WHERE id = $1`,
-        [cart.rows[i].id_producto]);
-        
-       await pool.query(
+        [cart.rows[i].id_producto]
+      );
+
+      await pool.query(
         `INSERT INTO detalle_pedido (id_pedido, id_producto, costo_unit, cantidad_prod, observaciones) VALUES ($1, $2, $3, $4, $5)`,
-        [newOrder.rows[0].id, cart.rows[i].id_producto, Product_Value.rows[0].cost_unit  ,cart.rows[i].cantidad_prod, cart.rows[i].observacion]
+        [
+          newOrder.rows[0].id,
+          cart.rows[i].id_producto,
+          Product_Value.rows[0].cost_unit,
+          cart.rows[i].cantidad_prod,
+          cart.rows[i].observacion,
+        ]
       );
     }
 
     // Actualizar los creditos del usuario
 
-    await pool.query(
-      `UPDATE datos_usuarios SET creditos = $1 WHERE id = $2`,
-      [credits_result, id]
-    );
+    await pool.query(`UPDATE datos_usuarios SET creditos = $1 WHERE id = $2`, [
+      credits_result,
+      id,
+    ]);
 
     res.json({ message: 'Pedido creado correctamente' });
-
   } catch (error) {
     console.error(error.message);
-    res
-      .status(500)
-      .json({ error: 'Ha ocurrido un error al crear el pedido' });
+    res.status(500).json({ error: 'Ha ocurrido un error al crear el pedido' });
   }
 }
 
 async function getByUserID(req, res) {
   try {
-
     const { id } = req.user;
     const response = await pool.query(
       `SELECT
@@ -224,11 +224,9 @@ async function getByUserID(req, res) {
 }
 
 async function getDetail(req, res) {
-
   try {
+    const { id_details_order } = req.query;
 
-    const {id_details_order} = req.query;
-  
     //Validar que los campos no estén vacíos
     if (!id_details_order) {
       return res
@@ -239,7 +237,6 @@ async function getDetail(req, res) {
     // Obtener los detalles del pedido
 
     const response = await pool.query(
-
       `SELECT  dp.id_pedido, p.fecha,
       pr.nombre, pr.descripcion, pr.img_product, dp.observaciones as ObservacionP, dp.costo_unit, dp.cantidad_prod, (dp.costo_unit * dp.cantidad_prod) as total_prod,
       d.departamento, d.ciudad, d.barrio, d.tipo_via, d.numero_via, d.numero_uno, d.numero_dos, d.observaciones as ObservacionD,
@@ -252,9 +249,9 @@ async function getDetail(req, res) {
       WHERE dp.id_pedido = $1`,
       [id_details_order]
     );
-    
+
     // Validar si el pedido no existe
-    
+
     if (response.rows.length === 0) {
       return res
         .status(404)
@@ -262,18 +259,18 @@ async function getDetail(req, res) {
     }
 
     res.json(response.rows);
-  }
-  catch (error) {
+  } catch (error) {
     console.error(error.message);
     res
       .status(500)
-      .json({ error: 'Ha ocurrido un error al obtener los detalles del pedido' });
-  } 
+      .json({
+        error: 'Ha ocurrido un error al obtener los detalles del pedido',
+      });
+  }
 }
 
 async function orderStatus(req, res) {
   try {
-
     const { id } = req.user;
     const { id_order, status } = req.body;
 
@@ -292,10 +289,19 @@ async function orderStatus(req, res) {
     }
 
     // Validar que el campo status tenga un valor válido
-    if (status !== 'Pendiente' && status !== 'Preparando' && status !== 'En camino' && status !== 'Entregado' && status !== 'Cancelado') {
+    if (
+      status !== 'Pendiente' &&
+      status !== 'Preparando' &&
+      status !== 'En camino' &&
+      status !== 'Entregado' &&
+      status !== 'Cancelado'
+    ) {
       return res
         .status(400)
-        .json({ error: 'El campo status debe ser Pendiente, Preparando, En camino, Entregado o Entregado' });
+        .json({
+          error:
+            'El campo status debe ser Pendiente, Preparando, En camino, Entregado o Entregado',
+        });
     }
 
     // Verificar si el pedido pertenece al restaurante
@@ -311,23 +317,24 @@ async function orderStatus(req, res) {
     }
 
     // Actualizar el estado del pedido
-    await pool.query(
-      `UPDATE pedido SET estado = $1 WHERE id = $2`,
-      [status, id_order]
-    );
+    await pool.query(`UPDATE pedido SET estado = $1 WHERE id = $2`, [
+      status,
+      id_order,
+    ]);
 
     res.json({ message: 'Estado del pedido actualizado correctamente' });
   } catch (error) {
     console.error(error.message);
     res
       .status(500)
-      .json({ error: 'Ha ocurrido un error al actualizar el estado del pedido' });
+      .json({
+        error: 'Ha ocurrido un error al actualizar el estado del pedido',
+      });
   }
 }
 
 async function orderRestaurant(req, res) {
   try {
-
     const { id } = req.user;
 
     response = await pool.query(
@@ -353,9 +360,7 @@ async function orderRestaurant(req, res) {
     }
 
     res.json(response.rows);
-  }
-    
-  catch (error) {
+  } catch (error) {
     console.error(error.message);
     res
       .status(500)
@@ -365,7 +370,6 @@ async function orderRestaurant(req, res) {
 
 async function getDetailsRestaurant(req, res) {
   try {
-
     const { id } = req.user;
     const { id_order } = req.query;
 
@@ -418,7 +422,10 @@ async function getDetailsRestaurant(req, res) {
       [id_order]
     );
 
-    if (response_data.rows.length === 0 || response_products.rows.length === 0) {
+    if (
+      response_data.rows.length === 0 ||
+      response_products.rows.length === 0
+    ) {
       return res
         .status(404)
         .json({ error: 'No se encontraron pedidos para el restaurante' });
@@ -426,13 +433,11 @@ async function getDetailsRestaurant(req, res) {
 
     json_response = {
       data: response_data.rows,
-      products: response_products.rows
-    }
+      products: response_products.rows,
+    };
 
     res.json(json_response);
-  }
-    
-  catch (error) {
+  } catch (error) {
     console.error(error.message);
     res
       .status(500)
@@ -440,4 +445,11 @@ async function getDetailsRestaurant(req, res) {
   }
 }
 
-module.exports = { add_order,getByUserID,getDetail,orderStatus,orderRestaurant, getDetailsRestaurant };
+module.exports = {
+  add_order,
+  getByUserID,
+  getDetail,
+  orderStatus,
+  orderRestaurant,
+  getDetailsRestaurant,
+};
